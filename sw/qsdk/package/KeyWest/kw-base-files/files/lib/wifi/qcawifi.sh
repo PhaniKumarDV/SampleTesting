5ea0f3c199d4c0f515566c410557979e1d8c5341
srilathagit <srilathagit@git.com>
2018-11-05 19:52:18 +0530

    Power configuration 1-26 dBm

--- sw/qsdk/package/KeyWest/kw-base-files/files/lib/wifi/qcawifi.sh
+++ sw/qsdk/package/KeyWest/kw-base-files/files/lib/wifi/qcawifi.sh
@@ -1901,7 +1901,7 @@ enable_qcawifi() {
 		config_get ddrsdectimer "$device" ddrsdectimer
 		config_get atpcstatus "$device" atpcstatus
 		config_get atpcpower "$device" atpcpower
-        pow=`expr $atpcpower + 3`
+        #pow=`expr $atpcpower + 3`
 
 
         iwpriv "$ifname" kwnstream "$spatialstream"
@@ -1916,7 +1916,7 @@ enable_qcawifi() {
             iwpriv "$ifname" kwnddrsmax "$ddrsrate"
         fi
         iwpriv "$ifname" kwnatpc "$atpcstatus"
-        iwpriv "$ifname" kwnatpcpow "$pow"
+        iwpriv "$ifname" kwnatpcpow "$atpcpower"
 
 
 		config_get kwnnoiseoff "$device" kwnnoiseoff
