42461b370eb7143eb2f0a8515c30a69edf5bf73a
srilathagit <srilathagit@git.com>
2019-03-23 17:44:56 +0530

    Changes for TXBA Setup issue

--- sw/qsdk/package/KeyWest/kw-base-files/files/usr/sbin/link.sh
+++ sw/qsdk/package/KeyWest/kw-base-files/files/usr/sbin/link.sh
@@ -8,6 +8,11 @@
 #iwpriv ath1 kwn_tput_dir 3
 #iwpriv ath1 kwn_tput_test 1
 
+# Commands executing for TXBA SETUP
+iwpriv ath1 kwn_tput_size 1400
+iwpriv ath1 kwn_tput_cnt 10
+iwpriv ath1 kwntxbasetup 1
+
 # Remove backup files for link inactivity and wireless inactivity
 rm -rf /tmp/.tx_wifipkt_cur 
 rm -rf /tmp/.tx_wifipkt_backup
