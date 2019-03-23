36c04dbcbfe06a36eaf61f0b64bf19cfa65b1aec
PhaniKumarDV <phani.dv@gmail.com>
2019-03-23 18:28:38 +0530

    added date in snmp traps

--- sw/qsdk/package/KeyWest/kw-base-files/files/usr/sbin/snmptrap.sh
+++ sw/qsdk/package/KeyWest/kw-base-files/files/usr/sbin/snmptrap.sh
@@ -1,5 +1,6 @@
 # Send SNMP Traps
 
+date=`date '+%a %b %Oe %H:%M:%S %Y'`
 trapmsg="TRAP Message"
 gentrapoid="1.3.6.1.4.1.52619.1.2.2.1"
 trapoid="1.3.6.1.4.1.52619.1.3.4.1"
@@ -14,49 +15,49 @@ trap_send()
 
 
 if [ "$1" -eq 1 ]; then
-    trapmsg="Associated (MAC:$2)"
+    trapmsg="$date: Associated (MAC:$2)"
     trapoid="1.3.6.1.4.1.52619.1.3.4.1"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 2 ]; then
-    trapmsg="Disassociated (MAC:$2)"
+    trapmsg="$date: Disassociated (MAC:$2)"
     trapoid="1.3.6.1.4.1.52619.1.3.4.2"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 3 ]; then
-    trapmsg="Remote Device (MAC:$2) is power off"
+    trapmsg="$date: Remote Device (MAC:$2) is power off"
     trapoid="1.3.6.1.4.1.52619.1.3.4.3"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 4 ]; then
-    trapmsg="Outdoor Base (MAC:$2) is power off"
+    trapmsg="$date: Outdoor Base (MAC:$2) is power off"
     trapoid="1.3.6.1.4.1.52619.1.3.4.4"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 5 ]; then
-    trapmsg="SU service is disabled"
+    trapmsg="$date: SU service is disabled"
     trapoid="1.3.6.1.4.1.52619.1.3.4.5"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 6 ]; then
-    trapmsg="Configuration applied successfully"
+    trapmsg="$date: Configuration applied successfully"
     trapoid="1.3.6.1.4.1.52619.1.3.4.6"
 	trap_send
     exit
 fi
 
 if [ "$1" -eq 7 ]; then
-    trapmsg="Device initialized successfully"
+    trapmsg="$date: Device initialized successfully"
     trapoid="1.3.6.1.4.1.52619.1.3.4.7"
 	trap_send
     exit
