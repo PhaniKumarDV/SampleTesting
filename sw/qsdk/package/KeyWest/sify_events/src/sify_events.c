89d81da11002a57ec52aa4db8e44656d12b728c4
srilathagit <srilathagit@git.com>
2019-03-25 21:45:25 +0530

    Distance calculation based on ACK Timeout

--- sw/qsdk/package/KeyWest/sify_events/src/sify_events.c
+++ sw/qsdk/package/KeyWest/sify_events/src/sify_events.c
@@ -1,5 +1,4 @@
 /*
-Author: viji 
 Sify Events
 */
 
@@ -8,6 +7,22 @@ Sify Events
 #include <sys/time.h>
 #include "sify_events.h"
 #include <math.h>
+#include <endian.h>
+#if __BYTE_ORDER == __BIG_ENDIAN
+#define _BYTE_ORDER _BIG_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#define _BYTE_ORDER _LITTLE_ENDIAN
+#else
+#error "__BYTE_ORDER undefined"
+#endif
+#define EXTERNAL_USE_ONLY
+#ifndef __packed
+#define __packed    __attribute__((__packed__))
+#endif
+#include "_ieee80211.h"
+#include "ieee80211_defines.h"
+#include "ieee80211.h"
+#include "ieee80211_ioctl.h"
 
 static void handle_newlink(struct ifinfomsg *ifi, unsigned char *buf, size_t len);
 static void handle_ifla_wireless ( char *data, int len);
@@ -29,6 +44,26 @@ int se_daemon = -1;
 #define IEEE80211_EV_ACS_START        52
 #define IEEE80211_EV_ACS_BEST_CHANNEL 53 
 #define IEEE80211_EV_SCAN_IN_PROGRESS 54
+
+#define pi 3.14159265358979323846
+#define KWN_DISTANCE_MIN              1
+#define KWN_DISTANCE_MAX              25
+#define KWN_CFG_BUF_LEN               650
+#define KWN_CMD_IMP_LEN               100
+#define KWN_CMD_OUT_LEN               50
+
+enum kwn_radiomode {
+    KWN_RADIOMODE_AP = 1,
+    KWN_RADIOMODE_SU = 2
+};
+
+enum kwn_linktype {
+    KWN_LINKTYPE_PTP = 1,
+    KWN_LINKTYPE_BH = 2,
+    KWN_LINKTYPE_PTMP_SMAC3 = 3,
+    KWN_LINKTYPE_PTMP_SMAC2 = 4
+};
+
 #define PRINTF(fmt, ...)\
 do\
 {\
@@ -38,6 +73,40 @@ do\
 }while(0)
 /* Sify Specific */
 void sify_file_write(char *,int,int,uint8_t);
+double deg2rad(double);
+double rad2deg(double);
+void get_assoclist( int );
+
+int distance(double lat1, double lon1, double lat2, double lon2) {
+    double theta, dist;
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        theta = lon1 - lon2;
+        dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+        dist = acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+        return (dist);
+    }
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts decimal degrees to radians             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double deg2rad(double deg) {
+    return (deg * pi / 180);
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts radians to decimal degrees             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double rad2deg(double rad) {
+    return (rad * 180 / pi);
+}
+
 void sify_file_write(char *sify_buf,int status, int reason, uint8_t value)
 {
 	time_t rawtime;
@@ -159,6 +228,27 @@ void sify_file_write(char *sify_buf,int status, int reason, uint8_t value)
 }
 /*end of sify */
 
+void kwn_sys_cmd_imp( const char* cmd, uint8_t* cmd_buf )
+{
+    FILE *fp = NULL;
+    uint8_t  a[KWN_CFG_BUF_LEN]={0};
+    uint8_t  *token;
+    uint16_t len;
+
+    fp = popen(cmd,"r");
+    if( fp == NULL )
+        return;
+    while( fgets(a, sizeof(a), fp) != NULL)
+    {
+        /*printf("%s",a);*/
+    }
+    pclose(fp);
+
+    token = strtok(a,"\n");
+    len = strlen(token);
+    memcpy(cmd_buf,token,len);
+    return;
+}
 /*track wireless events*/
 int init_wireless_tracking(int sock_fd)
 {
@@ -385,7 +475,8 @@ static void handle_ifla_wireless ( char *data, int len)
 				}
 				//syslog(LOG_INFO,"Wireless SU Expired:%s",mac);		       
 				sify_file_write(mac,0,iwe->u.addr.sa_data[6],iwe->u.addr.sa_data[7]);
-				break;
+                kwn_set_distance( KWN_DISTANCE_MAX );
+                break;
 			case IWEVREGISTERED:
 				PRINTF("[%s:%d] IWEVREGISTERED\r\n",__func__,__LINE__);
 				for (ix = 0; ix < 6; ++ix)
@@ -398,7 +489,8 @@ static void handle_ifla_wireless ( char *data, int len)
 					memset(tm_mac,'\0',3);
 				}
 				//syslog(LOG_INFO,"New Wireless SU Registered:%s",mac);
-				sify_file_write(mac,1,0,0);
+                sify_file_write(mac,1,0,0);
+                get_assoclist( KWN_RADIOMODE_AP );
 				break;
 			case IWEVASSOCREQIE:
 				break;
@@ -437,6 +529,7 @@ static void handle_ifla_wireless ( char *data, int len)
 						sify_file_write(prev_mac,0,0,0);
 						registered = 0;
 	                    system("iwpriv wifi1 qboost_enable 0");
+                        kwn_set_distance( KWN_DISTANCE_MAX );
 					}
 				}
 				else
@@ -448,6 +541,7 @@ static void handle_ifla_wireless ( char *data, int len)
 						sify_file_write(mac,1,0,0);
 						registered = 1;
                         system("/usr/sbin/link.sh > /dev/null 2>&1");
+                        get_assoclist( KWN_RADIOMODE_SU );
 					}
 				}
 				break;
@@ -459,4 +553,94 @@ static void handle_ifla_wireless ( char *data, int len)
 	PRINTF("\r\n[%s:%d] handle_ifla_wireless return \r\n",__func__,__LINE__);
 }
 
+void kwn_set_distance( int dist )
+{
+    char cmd[100];
+
+    dist = ( dist < KWN_DISTANCE_MIN ) ? KWN_DISTANCE_MIN : dist;
+    dist = ( dist > KWN_DISTANCE_MAX ) ? KWN_DISTANCE_MAX : dist;
+    sprintf(cmd,"iwpriv wifi1 distance %d",dist);
+    system( cmd );
+    system("iwpriv wifi1 pdev_reset 3");
+}
+
+void get_assoclist( int radiomode )
+{
+    int sk_fd = -1;
+    struct ieee80211req_sta_info *si;
+    struct iwreq iwr;
+    unsigned char *cp = NULL;
+    unsigned char buf[ 24 * 1024 ]  ={ 0 };
+    int len, num = 0;
+    char lat1[16], lon1[16], lat2[16], lon2[16];
+    char *st;
+    double l1, o1, l2, o2;
+    int dist;
+    char cmd[KWN_CMD_IMP_LEN];
+    uint8_t cmd_buf[KWN_CMD_OUT_LEN];
+    uint8_t linktype;
+
+    (void) memset( &iwr, 0, sizeof(iwr) );
+    (void) strncpy( iwr.ifr_name, "ath1", sizeof( iwr.ifr_name ) );
+    iwr.u.data.pointer = ( void * ) buf;
+    iwr.u.data.length = sizeof( buf );
+    sk_fd = socket( AF_INET, SOCK_DGRAM, 0 );
+
+    if( sk_fd < 0 ) {
+        sk_fd = -1;
+        return;
+    }
+    if( ioctl( sk_fd, IEEE80211_IOCTL_STA_INFO, &iwr) < 0 ) {
+        close( sk_fd );
+        return;
+    }
+    len = iwr.u.data.length;
+    if( len < sizeof( struct ieee80211req_sta_info ) ) {
+        close( sk_fd );
+        return;
+    }
+    cp = buf;
+    do {
+        si = ( struct ieee80211req_sta_info * ) cp;
+        memcpy( lat1, si->isi_l_latitude+5, sizeof( lat1 ) );
+        memcpy( lon1, si->isi_l_longitude+5, sizeof( lon1 ) );
+        memcpy( lat2, si->isi_r_latitude+5, sizeof( lat2 ) );
+        memcpy( lon2, si->isi_r_longitude+5, sizeof( lon2 ) );
+
+        l1 = strtod(lat1,&st);
+        o1 = strtod(lon1,&st);
+        l2 = strtod(lat2,&st);
+        o2 = strtod(lon2,&st);
+
+        dist = distance( l1, o1, l2, o2 );
+        printf("Distance : %d \n",dist);
+        num ++;
+        cp += si->isi_len, len -= si->isi_len;
+    } while( len >= sizeof( struct ieee80211req_sta_info ) );
+    close( sk_fd );
+    printf("Number of links %d \n",num);
+
+    if( num > 0 ) {
+        if( radiomode == KWN_RADIOMODE_SU ) {
+            kwn_set_distance( dist );
+        }
+        else {
+            /* Get Link Type */
+            memset(cmd, '\0', sizeof(cmd));
+            memset(cmd_buf, '\0', sizeof(cmd_buf));
+            sprintf(cmd,"uci get wireless.wifi1.linktype");
+            kwn_sys_cmd_imp( &cmd[0], &cmd_buf[0] ); 
+            linktype = atoi( cmd_buf );
+            if( linktype == KWN_LINKTYPE_PTP || linktype == KWN_LINKTYPE_BH ) {
+                kwn_set_distance( dist );
+            }
+            else {
+                kwn_set_distance( KWN_DISTANCE_MAX );
+            }
+        }
+    }
+    else {
+        kwn_set_distance( KWN_DISTANCE_MAX );
+    }
+}
 
